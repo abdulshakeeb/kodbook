921c92e49c372943433c2657bbd9720db200a184
shakeeb121 <abdulshakeeb22986@gmail.com>
2024-09-13 17:42:10 +0530

    My first commit

---
+++ com.kodbook/src/main/java/com/kodbook/services/UserService.java
@@ -0,0 +1,16 @@
+package com.kodbook.services;
+
+import com.kodbook.entities.User;
+
+public interface UserService {
+
+	void addUser(User user);
+
+	boolean userExists(String username, String email);
+
+	boolean validateUser(String username, String password);
+
+
+	
+
+}
