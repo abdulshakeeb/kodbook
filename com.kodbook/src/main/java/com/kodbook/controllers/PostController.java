9b29063fdc13290fea3dc12ebcc0f26be1149449
shakeeb121 <abdulshakeeb22986@gmail.com>
2024-09-17 17:25:54 +0530

    New changes

--- com.kodbook/src/main/java/com/kodbook/controllers/PostController.java
+++ com.kodbook/src/main/java/com/kodbook/controllers/PostController.java
@@ -1,9 +1,12 @@
 package com.kodbook.controllers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -27,6 +30,35 @@ public class PostController{
 			e.printStackTrace();
 		}
 		service.createPost(post);
+		return "home";
+	}
+	
+	@PostMapping("/likePost")
+	public String likePost(@RequestParam Long id, Model model) {
+		Post post= service.getPost(id);
+		post.setLikes(post.getLikes() + 1);
+		service.updatePost(post);
+		
+		List<Post> allPosts = service.fetchAllPosts();
+		model.addAttribute("allPosts", allPosts);
+		return "home";
+	}
+	
+	@PostMapping("/addComment")
+	public String addComment(@RequestParam Long id, 
+			@RequestParam String comment, Model model) {
+		System.out.println(comment);
+		Post post= service.getPost(id);
+		List<String> comments = post.getComments();
+		if(comments == null) {
+			comments = new ArrayList<String>();
+		}
+		comments.add(comment);
+		post.setComments(comments);
+		service.updatePost(post);
+		
+		List<Post> allPosts = service.fetchAllPosts();
+		model.addAttribute("allPosts", allPosts);
 		return "home";
 	}
 }
--- com.kodbook/src/main/java/com/kodbook/services/PostServiceImplementation.java
+++ com.kodbook/src/main/java/com/kodbook/services/PostServiceImplementation.java
@@ -1,5 +1,7 @@
 package com.kodbook.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,30 @@ public class PostServiceImplementation implements PostService{
 
 	@Override
 	public void createPost(Post post) {
+		// TODO Auto-generated method stub
+		repo.save(post);
+	}
+
+	@Override
+	public List<Post> getAllPosts() {
+		// TODO Auto-generated method stub
+		return repo.findAll();
+	}
+
+	@Override
+	public List<Post> fetchAllPosts() {
+		// TODO Auto-generated method stub
+		return repo.findAll();
+	}
+
+	@Override
+	public Post getPost(Long id) {
+		// TODO Auto-generated method stub
+		return repo.findById(id).get();
+	}
+
+	@Override
+	public void updatePost(Post post) {
 		// TODO Auto-generated method stub
 		repo.save(post);
 	}
